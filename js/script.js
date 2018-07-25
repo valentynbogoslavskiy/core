f31ab4f1ea97e55b16b16011f5843d643049a0b3
Bogoslavskiy valentyn <valentyn.bogoslavskiy@lemberg.co.uk>
2018-07-25 18:06:05 +0300

    Added styles and scripts for home page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,47 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  //activate mobile menu
+  if ($('.sidenav').length) {
+    $('.sidenav').sidenav();
+  }
+  //end activate mobile menu
 
+  //slider
+  if ($('.slider').length) {
+    $('.slider').slider({
+      fullWidth: true,
+      indicators: false
+    });
+    $('.next-button').click(function () {
+      $('.slider').slider('next');
+    });
+    $('.prev-button').click(function () {
+      $('.slider').slider('prev');
+    });
+  }
+  //end slider
+
+  //tabs
+  if ($('.tabs').length) {
+    $('.tabs').tabs();
+  }
+  //end tabs
+
+  $(window).scroll(function () {
+    //check small and large header
+    var sticky = $('.page-header'),
+      scroll = $(window).scrollTop();
+    if (scroll >= 100) sticky.addClass('small-header');
+    else sticky.removeClass('small-header');
+    //end check small and large header
+
+    //effect paralax
+    if ($('.slider').length ) {
+      $('.slider img').css({
+        '--y': (scroll + scroll/10) + 'px'
+      })
+    }
+    //end effect paralax
+
+  })
+  ;
 });
