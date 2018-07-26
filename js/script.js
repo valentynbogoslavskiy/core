97f84f4a31441cceb38d242dc0db0e3714f5f0e4
Bogoslavskiy valentyn <valentyn.bogoslavskiy@lemberg.co.uk>
2018-07-26 12:05:45 +0300

    Added animate menu

--- js/script.js
+++ js/script.js
@@ -20,28 +20,45 @@ $(document).ready(function () {
   }
   //end slider
 
+  //menu scroll
+  $('.header-menu a, .sidenav a').click(function (e) {
+    var target = $(this).attr('href'),
+      has = target.charAt(0);
+
+    if (has === '#' && $(target).length) {
+      e.preventDefault();
+      $('html, body').animate({scrollTop: $(target).offset().top - 25}, 1000);
+    }
+  });
+  //end menu scroll
+
   //tabs
   if ($('.tabs').length) {
     $('.tabs').tabs();
   }
   //end tabs
 
-  $(window).scroll(function () {
-    //check small and large header
+  //check small and large header
+  function sizeHeader() {
     var sticky = $('.page-header'),
       scroll = $(window).scrollTop();
     if (scroll >= 100) sticky.addClass('small-header');
     else sticky.removeClass('small-header');
-    //end check small and large header
+  }
 
+  sizeHeader();
+  //end check small and large header
+
+
+  $(window).scroll(function () {
+    sizeHeader();
     //effect paralax
     if ($('.slider').length && $(window).height() > scroll) {
       $('.slider img').css({
-        '--y': (scroll + scroll/15) + 'px'
+        '--y': (scroll + scroll / 15) + 'px'
       })
     }
     //end effect paralax
-
   })
   ;
 });
